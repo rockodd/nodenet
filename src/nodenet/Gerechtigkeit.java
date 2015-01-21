67243cd1e87be92ad1d306cf4c379a47218b4668
S827309 <S827309@SE4.swedom.virt>
2015-01-21 14:07:12 +0100

    ordnung verbessert

--- src/nodenet/Gerechtigkeit.java
+++ src/nodenet/Gerechtigkeit.java
@@ -8,51 +8,57 @@ package nodenet;
  *
  */
 public class Gerechtigkeit implements NodeBehavior {
-	
+
 	private Object paket = null;
 	private int kanalOutNr = 0, kanalInNr = 0;
-
-
 
 	@Override
 	public void transmitPacket(InputChannelVector inputChannels,
 			OutputChannelVector outputChannels) {
-	    
-	    if (paket != null){	schreiben(outputChannels);	}
-	    else {	lesen(inputChannels);	}
-	    
+
+		if (paket != null) {
+			schreiben(outputChannels);
+		} else {
+			lesen(inputChannels);
+		}
+
 	}
 
-	
-
-
-	//Methode um Pakete aus dem Kanal zu lesen
+	// Methode um Pakete aus dem Kanal zu lesen
 	private void lesen(InputChannelVector kanal) {
-		if ((kanal.size() == 0)){ return;	}
-	  	try {
-			paket = kanal.elementAt(kanalInNr).readObject(); //Packet empfangen
-		} 
-    	catch (ChannelEmptyException e) {System.out.println("Eingang leer");;	} 
-    	catch (ChannelDisabledException e) {System.out.println("Eingang deaktiviert");	}
-		
-		kanalInNr = (kanalInNr+1) % kanal.size();
+		if ((kanal.size() == 0)) {
+			return;
+		}
+		kanalInNr = (kanalInNr + 1) % kanal.size();
+		try {
+			paket = kanal.elementAt(kanalInNr).readObject(); // Packet empfangen
+		}
+
+		catch (ChannelEmptyException e) {
+			return;
+		}
+
+		catch (ChannelDisabledException e) {
+			return;
+		}
+
 	}
-	
-	
-	
+
 	// Methode zum schreiben in den Kanal
 	private void schreiben(OutputChannelVector kanal) {
-		if ((kanal.size() == 0)){ return;	}
-		
-        try {
-			kanal.elementAt(kanalOutNr).writeObject(paket);	//Paket versenden
-		} 
-	    catch (ChannelFullException e) {return;	} 
-	    catch (ChannelDisabledException e) {return;		} 
-	    paket = null;
-	    kanalOutNr = (kanalOutNr+1) % kanal.size();
+		if ((kanal.size() == 0)) {
+			return;
+		}
+		kanalOutNr = (kanalOutNr + 1) % kanal.size();
+		try {
+			kanal.elementAt(kanalOutNr).writeObject(paket); // Paket versenden
+		} catch (ChannelFullException e) {
+			return;
+		} catch (ChannelDisabledException e) {
+			return;
+		}
+		paket = null;
+
 	}
 
-		
 }
-		    
